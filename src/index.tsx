1a17ef6e7ff9586a19c9c5e5722cec18c00b5c5f
lixiping <lixp@sctek.com>
2019-10-31 14:30:24 +0800

    自动化管理域名

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,6 @@ import './index.css';
 import App from './App';
 import store from './store/index'
 import * as serviceWorker from './serviceWorker';
-console.log(store)
+console.log(process.env.NODE_ENV)
 ReactDOM.render(<App store={store}/>, document.getElementById('root'));
 serviceWorker.unregister();
